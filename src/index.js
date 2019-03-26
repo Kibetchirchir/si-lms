19852c80e460a6ba5acbf3d4a17b4847423c1ef9
Kelvin Chirchir <langatchirchir@gmail.com>
2019-03-26 12:34:33 +0300

    initiated the app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Send IT';
+
+ReactDOM.render(
+<div>{title}</div>,
+document.getElementById('app')
+);
+module.hot.accept();
